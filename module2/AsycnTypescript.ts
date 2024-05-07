7d92155f35cf6f338578142c9f63421573f603fd
Roman Sarker <romansarker2015@gmail.com>
2024-05-07 17:38:12 +0600

    async into typescript

---
+++ module2/AsycnTypescript.ts
@@ -0,0 +1,42 @@
+{
+    // promise 
+    //simulate
+
+
+
+   const createPromise = ():Promise<string> =>{ //we can make promise for object also
+ return new Promise<string>((resolve,reject) =>{
+   const data: string = "something"
+   if(data){
+    resolve(data)
+   }else{
+    reject('failed to load data')
+   }
+    })
+ }
+//calling create promise function
+
+const showData = async():Promise<string> =>{
+    const data : string= await createPromise()
+    return data
+    // console.log(data);
+}
+showData();
+
+//calling API Data
+type Todo = {
+    id: number;
+    userId: number;
+    title:string;
+    completed:boolean;
+}
+const getTodo = async()  : Promise<Todo> => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+
+    const data = await response.json()
+    return data;
+    // console.log(data);
+}
+getTodo()
+    //
+}
